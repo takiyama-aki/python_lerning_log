e6d8dcd85f8df257d2e41d0027a708f61d83e735
akki <takiyamaakihito@gmail.com>
2022-01-10 12:29:44 +0900

    githubにプッシュ

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
@@ -19,7 +19,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """1つのトピックとそれについてのすべての記事を表示"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # トピックが現在のユーザーが所持するものであることを確認する
     if topic.owner != request.user:
         raise Http404
